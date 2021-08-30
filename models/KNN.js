4352b9128c2af7823f1bd912991ca50b65954d51
TamirEliraz <80205308+TamirEliraz@users.noreply.github.com>
2021-08-30 15:32:38 +0300

    Create KNN.js
    first try, it gets an array of grades (of the student) and an array of arrays of grades(of the other students from the database) and returns k closest students

---
+++ models/KNN.js
@@ -0,0 +1,39 @@
+const a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] // sample data -> a should be the list of students in the database
+const b = [3, 4, 5] // sample student -> b should be the student's data that comes from the user-side
+/**
+ * KNN algo runs over ALL data, and returns k (default 2) students that has the closest grades to the "our" student
+ * @param data the list of students in the database
+ * @param new_student the student's data that comes from the user-side
+ * @param k number of closest students
+ * @returns {*[]} returns an array of k best-matched students
+ * @constructor
+ */
+function KNN(data, new_student, k = 2) {
+    let distances = [] // empty array of distances
+    for (let student = 0; student < data.length; student++) {
+        let d = distance(data[student], new_student)
+        distances[student] = [d, student] // add (x,y) where x is the distance and y is the student number/id
+    }
+    distances.sort() // sort
+    return distances.slice(0, k) // returns k best students
+}
+
+/**
+ * distance function, "straightforward", that to say Pitagoras in N dimes (=or "euclidean distance")
+ * @param a first number (as array)
+ * @param b second number (as array)
+ * @returns {number} float that is the distance between a to b
+ */
+function distance(a, b) {
+    let sum = 0
+    for (let i = 0; i < a.length; i++) {
+        sum += ((b[i] - a[i]) * (b[i] - a[i]))
+    }
+    return Math.sqrt(sum)
+}
+/*
+function main() { // simple just to check it works fine, as it is :)
+    console.log(KNN(a, b, 3))
+}
+
+main()*/
